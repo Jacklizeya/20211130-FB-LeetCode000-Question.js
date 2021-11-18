7724fecf684c0fc7e51467f1ec2649e1a8394920
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-11-17 18:02:05 -0700

    Update 20211117 LeetCode62 UniquePath.js

--- 20211117 LeetCode62 UniquePath.js
+++ 20211117 LeetCode62 UniquePath.js
@@ -17,3 +17,31 @@ var uniquePaths = function(m, n) {
         if (j+1 < n) {expand(i, j+1)}
     }
 };
+
+
+
+// Upgrade with Dynamic Programming M* N
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+
+    let matrix = []
+    matrix.push(Array(n).fill(1))
+    
+    for (let row  = 1; row < m; row++) {
+        matrix.push([1])
+        for (let col = 1; col < n; col++) {
+           matrix[row].push(matrix[row - 1][col] + matrix[row][col -1])
+        }
+    }
+
+    console.log(matrix)
+    return matrix[m-1][n-1]
+
+};
+
+console.log(uniquePaths(3, 7))
