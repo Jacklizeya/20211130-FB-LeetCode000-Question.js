5282466e05c9213ae9f9928f6bf9810b4e51695d
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-11-24 07:36:50 -0700

    Update 20211124 LeetCode140 WordBreak.js

--- 20211124 LeetCode140 WordBreak.js
+++ 20211124 LeetCode140 WordBreak.js
@@ -43,3 +43,71 @@ var wordBreak = function(s, wordDict) {
 console.log(wordBreak(s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]))
 console.log(wordBreak(s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]))
 console.log(wordBreak( s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]))
+
+
+
+
+// Improved solution, push and pop
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+var wordBreak = function(s, wordDict) {
+    let finalIndex = s.length
+    let result = []
+
+    let storage = {}
+    // build a hashmap store possible brick
+    for (let char of wordDict) {
+       let startingCharacter = char[0]
+       if (startingCharacter in storage) {storage[startingCharacter].push(char)}
+       else {storage[startingCharacter] = [char]}
+    }
+
+    graphTraverse(0, [])
+
+    return result
+
+    function graphTraverse(currentLocation = 0, path = []) {
+       
+        if (currentLocation > finalIndex) {
+            // path.pop();
+            return
+        }
+        if (currentLocation === finalIndex) {
+            result.push(path.join(" ")); 
+            // path.pop();
+            return
+            }
+        
+        let possiblePaths = storage[s[currentLocation]]
+       
+        if (possiblePaths) {
+                let qualifiedPath =  possiblePaths.filter(possiblePath => possiblePath === s.substring(currentLocation, currentLocation + possiblePath.length))
+                if (qualifiedPath.length > 0) {
+                    qualifiedPath.forEach((possiblePath)=>{
+                        // let newPath = [...path]
+                        // newPath.push(possiblePath)
+                        // graphTraverse(currentLocation + possiblePath.length, newPath)
+
+                        path.push(possiblePath)
+                        graphTraverse(currentLocation + possiblePath.length, path)
+                        path.pop();
+                    })
+                } else {
+                    // path.pop();
+                    return
+                }
+        } else {
+            // path.pop();
+            return
+        }    
+    }
+
+};
+
+// console.log(wordBreak(s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]))
+// console.log(wordBreak(s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]))
+// console.log(wordBreak( s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]))
