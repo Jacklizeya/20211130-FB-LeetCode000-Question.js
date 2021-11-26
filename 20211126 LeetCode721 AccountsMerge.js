7216e0c2505540579438f6bdc8e6ea06321a25d7
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-11-26 16:01:12 -0700

    Update 20211126 LeetCode721 AccountsMerge.js

--- 20211126 LeetCode721 AccountsMerge.js
+++ 20211126 LeetCode721 AccountsMerge.js
@@ -88,3 +88,80 @@ var accountsMerge = function(accounts) {
     ["David","David1@m.co","David1@m.co","David8@m.co"],
     ["David","David0@m.co","David0@m.co","David5@m.co"]]
  ))
+
+
+
+// Solution is getting faster, but not faster enough to deal with large data sets
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    // O of N, store in a hash map
+    let storage = {} 
+    for (let account of accounts) {
+        let name = account[0]
+        let emails = account.slice(1, account.length)
+        if (!storage[name]) {storage[name] = [emails]}
+        else {storage[name].push(emails)}
+    }
+    // console.log("step 1 organize into hashmap", storage)
+    let result = []
+    for (let name in storage) {
+        let nameRelatedArrays = storage[name]
+        mergeArrays(nameRelatedArrays)
+        for (let mergedEmailArray of nameRelatedArrays) {
+            let mergedEmailSet = new Set(mergedEmailArray)
+            result.push([name, ...[...mergedEmailSet].sort()])
+        }
+    } 
+    return result
+
+
+    function mergeArrays(stringIntervals) {
+        let result = []
+        for (let i = 0; i < stringIntervals.length - 1; i++) {
+            for (let j = i + 1; j < stringIntervals.length; j++) {
+                if (canMerge(stringIntervals[i], stringIntervals[j])) { stringIntervals[i] = mergeInterval(stringIntervals[i], stringIntervals[j]); stringIntervals.splice(j, 1); i = 0; j = 0}
+            }
+        }
+        return result
+    }
+
+    function canMerge(array1, array2) {
+        let set1 = new Set(...array1)
+        let set2 = new Set(...array2)
+        for (let email of set1) {
+            if (set2.has(email)) {return true}
+        }
+        return false
+    }
+
+    function mergeInterval(array1, array2) {
+        // converting array to set so we can merge it faster
+        // console.log("arrays", array1, array2)
+        let set1 = new Set(array1)
+        let set2 = new Set(array2)
+        // console.log("sets", set1, set2)
+        for (let element of set2) {
+            if (!set1.has(element)) {set1.add(element)}
+        }
+        return [...set1]
+    }
+ };
+
+
+//  console.log(accountsMerge(
+//     [["Alex","Alex5@m.co","Alex4@m.co","Alex0@m.co"],
+//     ["Ethan","Ethan3@m.co","Ethan3@m.co","Ethan0@m.co"],
+//     ["Kevin","Kevin4@m.co","Kevin2@m.co","Kevin2@m.co"],
+//     ["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe2@m.co"],
+//     ["Gabe","Gabe3@m.co","Gabe4@m.co","Gabe2@m.co"]]
+//  ))
+
+ console.log(accountsMerge(
+    [["David","David5@m.co","David8@m.co"],
+    ["David","David1@m.co","David1@m.co","David8@m.co"],
+    ["David","David0@m.co","David0@m.co","David5@m.co"]]
+ ))
+
