499e18bd2cb345a75daa5871bf604c079d51f0ab
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-11-30 17:32:24 -0700

    Update 20211130 FB.js

--- 20211130 FB.js
+++ 20211130 FB.js
@@ -9,11 +9,25 @@ Q2
 Two pointers based on the assumption all numbers are integer: continuous sum problem
 There is some hard Logic here
 
-while (rightPointer < length) {
-if (sum === target) {return true}
-else if (sum < target) {rightPointer++ ; sum += array[rightPointer]}
-else {
-  if (leftPointer < rightPointer) { sum -= array[leftPointer]; leftPointer++}
-  else {rightPointer++ ; sum += array[rightPointer]} 
+// constrain is it could be just one, and all numbers are positive
+
+function check(array, target) {
+    let leftPointer = 0
+    let rightPointer = 0
+    let sum = array[0]
+
+    while (rightPointer < array.length) {
+        console.log("sum", sum)
+        if (sum === target) {return true}
+        else if (sum < target) {rightPointer++ ; sum += array[rightPointer]}
+        else {
+          if (leftPointer < rightPointer) { sum -= array[leftPointer]; leftPointer++}
+        //   if (leftPointer === rightPointer && rightPointer === array.length - 1) {return false}
+          else {rightPointer++ ; sum += array[rightPointer]} 
+        }
+    }
+
+return false
 }
-}
+
+console.log(check([1, 2, 11, 4, 10], 10))
